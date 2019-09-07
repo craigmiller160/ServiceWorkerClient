022a3a52f727e1c1968c4cd7da8e8c9f570c1503
Craig Miller <craigmiller160@gmail.com>
2019-09-07 19:21:18 -0400

    Got the auth header working

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ const loadTxtBtn = document.querySelector('#load-text-btn');
 const serverTxt = document.querySelector('#server-text');
 
 loadTxtBtn.onclick = () => {
-    axios.get('http://localhost:3000/hello')
+    axios.get('http://localhost:3000/hello', {
+        headers: {
+            authorization: 'Hello There'
+        },
+        withCredentials: true
+    })
         .then((res) => {
             serverTxt.textContent = res.data;
         })
