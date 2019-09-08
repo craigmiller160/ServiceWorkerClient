d1178215badd4a088c5f710bb5b71a02e0eeac3a
Craig Miller <craigmiller160@gmail.com>
2019-09-08 16:32:16 -0400

    Got some basic authentication logic working

--- Auth/main.js
+++ Auth/main.js
@@ -11,6 +11,31 @@ const instance = axios.create({
     withCredentials: true
 });
 
+const configureContentPage = () => {
+    const loadTxtBtn = document.querySelector('#load-text-btn');
+    const serverTxt = document.querySelector('#server-text');
+    loadTxtBtn.onclick = async () => {
+        const res = await instance.get('/hello');
+        serverTxt.textContent = res.data;
+    };
+};
+
+const configureLoginPage = () => {
+    const usernameInput = document.querySelector('#username');
+    const passwordInput = document.querySelector('#password');
+    const loginBtn = document.querySelector('#login-btn');
+
+    loginBtn.onclick = async () => {
+        const username = usernameInput.value;
+        const password = passwordInput.value;
+        const res = await instance.post('/login', {
+            username,
+            password
+        });
+        console.log(res.data); // TODO delete this
+    };
+};
+
 const showPage = (pageName) => {
     let firstChild = wrapper.firstChild;
     while (firstChild) {
@@ -20,15 +45,11 @@ const showPage = (pageName) => {
 
     if (LOGIN_PAGE === pageName) {
         wrapper.append(loginPage.content.cloneNode(true));
+        configureLoginPage();
     }
     else {
         wrapper.append(contentPage.content.cloneNode(true));
-        const loadTxtBtn = document.querySelector('#load-text-btn');
-        const serverTxt = document.querySelector('#server-text');
-        loadTxtBtn.onclick = async () => {
-            const res = await instance.get('/hello');
-            serverTxt.textContent = res.data;
-        };
+        configureContentPage();
     }
 };
 
