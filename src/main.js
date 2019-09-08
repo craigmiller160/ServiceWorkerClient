6b569219da41eaf7d12483262866613b906b5d06
Craig Miller <craigmiller160@gmail.com>
2019-09-08 12:04:39 -0400

    Registered the service worker

--- src/main.js
+++ src/main.js
@@ -17,3 +17,14 @@ loadTxtBtn.onclick = () => {
             alert('Error with loading text');
         });
 };
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js')
+            .then((registration) => {
+                console.log(`ServiceWorker registered`, registration);
+            }, (error) => {
+                console.log('ServiceWorker registration failed', error);
+            });
+    });
+}
